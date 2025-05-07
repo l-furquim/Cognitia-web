fcea8374bda574ac88fca88b810c47a0aa1a88d4
l-furquim <lucas.furquim@sptech.school>
2025-05-07 18:21:16 -0300

    add a better area selection

--- src/app/(auth)/register/components/area-selector.tsx
+++ src/app/(auth)/register/components/area-selector.tsx
@@ -2,19 +2,45 @@ import {
   ToggleGroup,
   ToggleGroupItem,
 } from "@/components/ui/toggle-group"
+import { useState } from "react";
 
 export const AreaSelector = () => {
+  const [areas, setAreas] = useState([]);
+
   return (
-    <ToggleGroup className="w-75 space-x-10" type="multiple">
-      <ToggleGroupItem value="bold" aria-label="Toggle bold">
-        Desenvolvimento de software
-      </ToggleGroupItem>
-      <ToggleGroupItem value="italic" aria-label="Toggle italic">
-        Gestão de projetos
-      </ToggleGroupItem>
-      <ToggleGroupItem value="strikethrough" aria-label="Toggle strikethrough">
-        Backend
-      </ToggleGroupItem>
+    <ToggleGroup onValueChange={(value) => setAreas(areas)} className="w-80 flex flex-col justify-center space-x-12" type="multiple">
+     <div className="flex">
+      <ToggleGroupItem className=" px-8 w-full" value="bold" aria-label="Toggle bold">
+          Desenvolvimento de software
+        </ToggleGroupItem>
+        <ToggleGroupItem className=" w-full" value="italic" aria-label="Toggle italic">
+          Gestão de projetos
+        </ToggleGroupItem>
+     </div>
+      <div className="flex">
+        <ToggleGroupItem className=" w-full" value="backend" aria-label="Toggle strikethrough">
+          Backend
+        </ToggleGroupItem>
+        <ToggleGroupItem className=" w-full" value="frontend" aria-label="Toggle strikethrough">
+          Frontend
+        </ToggleGroupItem>
+      </div>
+      <div className="flex">
+        <ToggleGroupItem className=" w-full" value="devops" aria-label="Toggle strikethrough">
+          Devops
+        </ToggleGroupItem>
+        <ToggleGroupItem className=" w-full" value="ingles" aria-label="Toggle strikethrough">
+          Inglês
+        </ToggleGroupItem>
+      </div>
+      <div className="flex">
+        <ToggleGroupItem className="px-8 w-full" value="machineLearning" aria-label="Toggle strikethrough">
+          Machine Learning
+        </ToggleGroupItem>
+        <ToggleGroupItem className=" w-full" value="java" aria-label="Toggle strikethrough">
+          Java
+        </ToggleGroupItem>
+      </div>
     </ToggleGroup>
   )
 }
